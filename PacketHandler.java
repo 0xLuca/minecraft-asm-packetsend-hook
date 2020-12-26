244d58829df8e3821a15ce6f4c06968f5bbb2051
Luca <59053718+luca-gg@users.noreply.github.com>
2020-12-26 09:07:24 +0100

    Create PacketHandler.java

---
+++ PacketHandler.java
@@ -0,0 +1,8 @@
+import net.minecraft.network.Packet;
+
+public class PacketHandler {
+    public static boolean shouldSendPacket(Packet<?> packet) {
+        //Do stuff here
+        return true;
+    }
+}
